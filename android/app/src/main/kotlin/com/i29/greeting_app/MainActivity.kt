62004529ee508a86a6f6933a15f3d265dd3a2ea7
smri29 <22203174@iubat.edu>
2024-09-18 11:08:30 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/i29/greeting_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.i29.greeting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+// Current SDK version: 3.22.1
+import 'package:flutter/material.dart';
+
+// Container, dialog, bottom-sheet
+
+void main() {
+  runApp(GreetingApp());
+}
+
+class GreetingApp extends StatelessWidget {
+  const GreetingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Home(),
+      title: 'Greeting App',
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Greeting App'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello, World!',
+              style: TextStyle(
+                color: Colors.deepOrange,
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+              ),),
+            Text('Welcome to Flutter!', style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+            ),),
+            SizedBox(height: 10,),
+            Image(image: AssetImage('asset/img/flutter.png')),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    barrierColor: Colors.black54,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: Text('Next'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Do you want to proceed?'),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              },
+                            child: Text('Next'),
+                          )
+                        ],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        backgroundColor: Colors.white,
+                      );
+                    });
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text('Press Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
